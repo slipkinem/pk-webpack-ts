01d50424167bf9c0d5e517c8f3cdaaa0c53d09ca
slipkinem <slipkinem@gmail.com>
2018-01-09 21:07:58 +0800

    change main.ts tslint disabled

--- template/src/main.ts
+++ template/src/main.ts
@@ -11,7 +11,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+/* tslint:disable:no-unused-expression */
 new Vue({
   el: '#app',
   {{#router}}
@@ -25,3 +25,4 @@ new Vue({
   components: { App }
   {{/if_eq}}
 })
+/* tslint:disable:no-unused-expression */
